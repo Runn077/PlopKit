416199423ed0b2382ee2676e834d0f030b7a9596
Runn077 <rupu0777@gmail.com>
2026-05-10 13:20:49 -0600

    feat: widget mounts relative to script tag for container support

--- widget/src/main.tsx
+++ widget/src/main.tsx
@@ -1,20 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './widgtes/comments/Comments'
+import Comments from './widgtes/comments/Comments'
 
 const script = document.currentScript as HTMLScriptElement
 const siteKey = script?.getAttribute('data-site-key') ?? ''
+const widget = script?.getAttribute('data-widget') ?? 'comments'
 
 const host = document.createElement('div')
-document.body.appendChild(host)
+host.style.width = '100%'
+script.parentNode?.insertBefore(host, script.nextSibling)
 
 const shadow = host.attachShadow({ mode: 'open' })
-
 const mountPoint = document.createElement('div')
 shadow.appendChild(mountPoint)
 
-createRoot(mountPoint).render(
-  <StrictMode>
-    <App siteKey={siteKey} pageUrl={window.location.href} />
-  </StrictMode>
-)
+const widgetMap: Record<string, React.ComponentType<any>> = {
+  comments: Comments,
+}
+
+const Widget = widgetMap[widget]
+
+if (Widget) { 
+  createRoot(mountPoint).render(
+    <StrictMode>
+      <Widget siteKey={siteKey} pageUrl={window.location.href} />
+    </StrictMode>
+  )
+}
