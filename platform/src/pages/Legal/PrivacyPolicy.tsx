02f84f1a7363b57466dbf5198d42a74c607639d6
Runn077 <rupu0777@gmail.com>
2026-06-15 01:07:19 -0600

    chore: add footer and navbar

--- platform/src/pages/Legal/PrivacyPolicy.tsx
+++ platform/src/pages/Legal/PrivacyPolicy.tsx
@@ -1,11 +1,17 @@
 import ReactMarkdown from 'react-markdown'
 import content from './PrivacyPolicy.md?raw'
 import './PrivacyPolicy.css'
+import LandingNavbar from '../../components/LandingNavbar'
+import Footer from '../../components/Footer'
 
 export default function PrivacyPolicy() {
   return (
+    <>
+    <LandingNavbar/>
     <div className="legal-page">
       <ReactMarkdown>{content}</ReactMarkdown>
     </div>
+    <Footer/>
+    </>
   )
 }
--- platform/src/pages/Legal/Terms.tsx
+++ platform/src/pages/Legal/Terms.tsx
@@ -1,11 +1,17 @@
 import ReactMarkdown from 'react-markdown'
 import content from './TermsAndServices.md?raw'
 import './Terms.css'
+import LandingNavbar from '../../components/LandingNavbar'
+import Footer from '../../components/Footer'
 
 export default function Terms() {
   return (
+    <>
+    <LandingNavbar/>
     <div className="legal-page">
       <ReactMarkdown>{content}</ReactMarkdown>
     </div>
+    <Footer/>
+    </>
   )
 }
